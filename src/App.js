ef7c884c8a176f76d1d8344044aa4e907d9e786b
Thail3 <tanai.saeng@gmail.com>
2022-04-10 11:51:17 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("");
+
+  const increaseNumber = () => {
+    setCount(count + 1);
+    if (count > 0) {
+      setColor("#73E31A");
+    } else if (count < 0) {
+      setColor("#FC2510");
+    }
+  };
+
+  const decreaseNumber = () => {
+    setCount(count - 1);
+    if (count < 0) {
+      setColor("#FC2510");
+    } else if (count > 0) {
+      setColor("#73E31A");
+    }
+  };
+
+  const resetNumber = () => {
+    setCount(0);
+    if (count === 0) {
+      setColor("white");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="App-header">
+        <div className="container">
+          <h3>Counter</h3>
+          <h1 style={{ color: `${color}` }}>{count}</h1>
+          <div>
+            <button onClick={increaseNumber}>INCREASE</button>
+            <button onClick={decreaseNumber}>DECREASE</button>
+            <button onClick={resetNumber}>RESET</button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
